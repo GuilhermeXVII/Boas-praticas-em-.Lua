88ae43cf5b8f6d7091c107c301a834fc8b8cbd8b
𝐺𝑢𝑖𝑙ℎ𝑒𝑟𝑚𝑒 <150200952+GuilhermeXVII@users.noreply.github.com>
2025-06-14 12:56:37 -0300

    Condicionais em lua

---
+++ SintaxeBásica/condicional.lua
@@ -0,0 +1,14 @@
+nome = "Guilherme"
+idade = 17
+altura = 160
+
+print("Seu nome é " .. nome .. ", sua idade é " .. idade .. " anos, você tem: " .. altura .. " metros")
+
+
+if idade >= 18 then
+	print(nome .. " é maior de idade")
+
+else
+	print(nome .. " é menor de idade")
+
+end
